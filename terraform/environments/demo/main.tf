b1e3f2442162ba8c273e2e253280acd0a9b8f47a
Martin <hojland93@gmail.com>
2023-02-28 19:50:36 +0000

    lower memory

--- terraform/environments/demo/main.tf
+++ terraform/environments/demo/main.tf
@@ -144,7 +144,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "c2d-highcpu-4"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.demo.email
